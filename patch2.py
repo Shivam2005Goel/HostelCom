b51f381241b3d6303989c1108fea65c4a32b3f31
Nishtha Jain <Nishtha@Prathams-MacBook-Air.local>
2026-04-04 02:52:09 +0530

    Cleanup extra files

--- patch2.py
+++
@@ -1,9 +0,0 @@
-with open('src/app/help/page.tsx', 'r') as f:
-    text = f.read()
-
-text = text.replace('const getScore = (name) =>', 'const getScore = (name: string) =>')
-text = text.replace('const max = (a, b) =>', 'const max = (a: number, b: number) =>')
-
-with open('src/app/help/page.tsx', 'w') as f:
-    f.write(text)
-
--- patch3.py
+++
@@ -1,7 +0,0 @@
-with open('src/app/help/page.tsx', 'r') as f:
-    text = f.read()
-
-text = text.replace('const requestRef = useRef<number>();', 'const requestRef = useRef<number>(0);')
-
-with open('src/app/help/page.tsx', 'w') as f:
-    f.write(text)
